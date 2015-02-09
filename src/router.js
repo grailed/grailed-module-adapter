261c5573701f5494d02fc88882bcea25c1ce49ca
Glen Arrowsmith <glen.arrowsmith@gmail.com>
2015-02-09 14:07:06 +1000

    Removes silent errors when requiring routes

--- src/router.js
+++ src/router.js
@@ -1,40 +1,37 @@
-module.exports = function ( _module ) {
-	var _ = require( 'underscore' ),
-		is = require( 'sc-is' ),
-		module = _module,
-		express = require( 'express' ),
-		path = require( 'path' ),
-		routes,
-		router = express.Router();
+module.exports = function(_module) {
+  var _ = require('underscore'),
+    is = require('sc-is'),
+    module = _module,
+    express = require('express'),
+    path = require('path'),
+    routes,
+    router = express.Router();
 
-	try {
-		routes = require( path.join( module.options.dirname, '../routes' ) )( module );
-	} catch ( e ) {
-	}
+  routes = require(path.join(module.options.dirname, '../routes'))(module);
 
-	if ( routes && !Array.isArray( routes ) ) throw new Error( 'The routes for the module ' + module.name + ' is not a valid Array.' );
+  if (routes && !Array.isArray(routes)) throw new Error('The routes for the module ' + module.name + ' is not a valid Array.');
 
-	if ( routes ) {
-		routes.forEach( function ( _route ) {
-			Object.keys( _route ).forEach( function ( _verb ) {
-				var route = _route[ _verb ],
-					errorString = 'The route `' + _verb.toUpperCase() + ' ' + _route.path + '` for the module `' + module.name + '` was not an Array or Function';
+  if (routes) {
+    routes.forEach(function(_route) {
+      Object.keys(_route).forEach(function(_verb) {
+        var route = _route[_verb],
+          errorString = 'The route `' + _verb.toUpperCase() + ' ' + _route.path + '` for the module `' + module.name + '` was not an Array or Function';
 
-				if ( _verb === 'path' ) return;
+        if (_verb === 'path') return;
 
-				if ( is.not.an.array( route ) && is.not.a.func( route ) )
-					throw new Error( errorString );
+        if (is.not.an.array(route) && is.not.a.func(route))
+          throw new Error(errorString);
 
-				if ( is.an.array( route ) ) {
-					_.each( route, function ( _middleware ) {
-						if ( is.not.a.func( _middleware ) ) throw new Error( errorString );
-					} );
-				}
+        if (is.an.array(route)) {
+          _.each(route, function(_middleware) {
+            if (is.not.a.func(_middleware)) throw new Error(errorString);
+          });
+        }
 
-				router[ _verb ]( _route.path, route );
-			} );
-		} );
-	}
+        router[_verb](_route.path, route);
+      });
+    });
+  }
 
-	return router;
+  return router;
 };
